8248ea4be81214b2c4d9821cca688f0b5fa22f35
rodrigo9827 <rlpplr1533@gmail.com>
2026-06-22 15:27:01 -0300

    update start

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 :: arquivo de inicialização dos atalhos para windows
 @echo off
-start %userprofile%\Downloads\atalhoos-de-teclado-geosaude-main\atalhoos-de-teclado-geosaude-main\dist\distribuicao_crt.exe
+"%userprofile%\Downloads\atalhoos-de-teclado-geosaude-main\atalhoos-de-teclado-geosaude-main\dist\distribuicao_crt.exe"
 pause
